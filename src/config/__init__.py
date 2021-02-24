00d596d49611266e64526c59cc02c7aa4af0e4c2
shoor <shoorday@outlook.com>
2021-02-24 21:18:49 +0800

    add dotenv config

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -3,21 +3,22 @@ from pathlib import Path
 
 from pydantic import BaseSettings
 
+BASE_DIR: Path = Path(__file__).resolve(strict=True).parent.parent.parent
+
 
 class Settings(BaseSettings):
-    BASE_DIR: Path = Path(__file__).resolve(strict=True).parent.parent.parent
     SECRET_KEY: str = "secrets.token_urlsafe(32)"
-    # 60 minutes * 24 hours * 7 days = 7 days
+
     ACCESS_TOKEN_LIFETIME: timedelta = timedelta(minutes=30)
     REFRESH_TOKEN_LIFETIME: timedelta = timedelta(days=30)
     JWT_ALGORITHM: str = "HS256"
 
-    # SQLALCHEMY_DATABASE_URI: str = f"sqlite:///{BASE_DIR.as_posix()}/db.sqlite3"
-    # SQLALCHEMY_DATABASE_URI = "postgresql://user:password@postgresserver/db"
-    SQLALCHEMY_DATABASE_URI = "postgresql://webdev:123456@127.0.0.1:5432/celery_tasks"
+    SQLALCHEMY_DATABASE_URI: str = f"sqlite:///{BASE_DIR.as_posix()}/db.sqlite3"
 
     class Config:
         case_sensitive = True
+        env_file = BASE_DIR / ".env"
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
